1c2835be2bbac548ae47c3a486320f6face0cb2e
LNA <LAllen@8thLight.com>
2015-02-16 12:49:41 -0600

    Gives response head in correct order

--- src/javax/com/lallen/httpserver/routing/GetRouter.java
+++ src/javax/com/lallen/httpserver/routing/GetRouter.java
@@ -2,8 +2,18 @@ package javax.com.lallen.httpserver.routing;
 import java.io.IOException;
 
 public class GetRouter implements iRouter {
+    public static final String ALLOW = "Allow: GET,HEAD,POST,OPTIONS,PUT\r\n";
+    public static final String STATUS = "HTTP/1.1 200 OK\r\n";
+    public static final String LOCATION = "Location: http://localhost:5000/" + "\r\n";
+    public static final String BLANK_LINE = "\r\n";
+
     public byte[] buildResponseHead() throws IOException {
-        String allow = "Allow: GET,HEAD,POST,OPTIONS,PUT\r\n";
-        return allow.getBytes();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(ALLOW);
+        stringBuilder.append(STATUS);
+        stringBuilder.append(LOCATION);
+        stringBuilder.append(BLANK_LINE);
+        String lines = stringBuilder.toString();
+        return lines.getBytes();
     }
 }
--- test/javax/com/lallen/httpserver/routing/GetRouterTest.java
+++ test/javax/com/lallen/httpserver/routing/GetRouterTest.java
@@ -1,17 +1,44 @@
 package javax.com.lallen.httpserver.routing;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 
 public class GetRouterTest {
     public static final String UTF = "UTF-8";
+    public static final String ALLOW = "Allow: GET,HEAD,POST,OPTIONS,PUT\r\n";
+    public static final String STATUS = "HTTP/1.1 200 OK\r\n";
+    public static final String LOCATION = "Location: http://localhost:5000/" + "\r\n";
+    public static final String BLANK_LINE = "\r\n";
+    public static final String HEAD_LINES = ALLOW + STATUS + LOCATION + BLANK_LINE;
+    public GetRouter getRouter;
+    public byte[] response;
+    public String head;
+
+    @Before
+    public void setUp() throws IOException {
+        getRouter = new GetRouter();
+        response = getRouter.buildResponseHead();
+        head = new String(response, UTF);
+    }
 
     @Test
     public void itGivesAllowForTheHeader() throws IOException {
-        GetRouter getRouter = new GetRouter();
-        byte[] response = getRouter.buildResponseHead();
-        String head = new String(response, UTF);
+        assertEquals(true, head.contains(ALLOW));
+    }
 
-        assertEquals(true, head.contains("Allow: GET,HEAD,POST,OPTIONS,PUT\r\n"));
+    @Test
+    public void itGivesTheStatusLine() throws IOException {
+        assertEquals(true, head.contains(STATUS));
+    }
+
+    @Test
+    public void itGivesTheLocation() throws IOException {
+        assertEquals(true, head.contains(LOCATION));
+    }
+
+    @Test
+    public void itGivesTheHeaderInTheCorrectOrder() throws IOException {
+        assertEquals(HEAD_LINES, head);
     }
 }
