4f9771f6a55dc45fd2974fda92645571ea01e0a6
LNA <LAllen@8thLight.com>
2015-02-23 10:59:26 -0600

    [REFACTOR] Makes routing function more readable

--- src/javax/com/lallen/httpserver/routing/Router.java
+++ src/javax/com/lallen/httpserver/routing/Router.java
@@ -1,5 +1,6 @@
 package javax.com.lallen.httpserver.routing;
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public class Router {
@@ -12,12 +13,20 @@ public class Router {
     public String sendToRoute() {
         if (request.get("URI").equals("REDIRECT")) {
             return "REDIRECT";
-        } else if (new File(request.get("PATH")).isFile() && request.get("VERB").equals("PATCH")) {
+        } else if (patchFile()) {
             return "PatchFileRouter";
-        } else if (new File(request.get("PATH")).isFile() && request.get("VERB").equals("GET")) {
+        } else if (getFile()) {
             return "GetFileRouter";
         } else {
             return request.get("VERB");
         }
+    }
+
+    private boolean patchFile() {
+        return new File(request.get("PATH")).isFile() && request.get("VERB").equals("PATCH");
+    }
+
+    private boolean getFile() {
+        return new File(request.get("PATH")).isFile() && request.get("VERB").equals("GET");
     }
 }
